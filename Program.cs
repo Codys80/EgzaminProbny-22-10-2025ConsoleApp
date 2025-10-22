71e0e1401f2df18486a4f746566043e3ecbc1b7a
CodyS80 <bartolosemolo@gmail.com>
2025-10-22 09:59:29 +0200

    Dodanie dokumentacji

--- Program.cs
+++ Program.cs
@@ -3,13 +3,7 @@
     internal class Program
     {
         static bool[] tablica = new bool[10000];
-         static void zapelnijTablice()
-        {
-            for (int i = 1; i < tablica.Length; i++)
-            {
-                tablica[i] = false;
-            }
-        }
+
         static bool czyWystepujeCztery(int index)
         {
             return index.ToString().Contains('4');
@@ -18,13 +12,11 @@
         {
             string indexString = index.ToString();
             int suma = 0;
-            int czworkowosc = 0;
             for (int i = 0; i<indexString.Length; i++)
             {
                 suma += int.Parse(indexString[i].ToString());
             }
             string indexSuma = suma.ToString();
-            //Console.WriteLine(suma);
             if (indexSuma[indexSuma.Length-1] == '4')
                 return true;
             return false;
@@ -32,7 +24,6 @@
 
         static void Main(string[] args)
         {
-            //czyLiczbaCzworkowa(13);
             for (int i = 1; i < tablica.Length; i++)
             {
                 tablica[i] = false;
@@ -46,13 +37,6 @@
                 }
                 
             }
-
-            for (int i = 1; i < tablica.Length; i++)
-            {
-
-                Console.WriteLine("i: "+i +" T: " + tablica[i]);
-            }
-            Console.WriteLine(czyLiczbaCzworkowa(68));
 
         }
     }
